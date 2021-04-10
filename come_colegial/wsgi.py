089c2735c0c461c040357aab31ce68b0b8e46626
paolagonzalez38 <70416959+paolagonzalez38@users.noreply.github.com>
2021-04-10 14:34:18 -0400

    Django folder boilerplate

---
+++ come_colegial/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for come_colegial project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'come_colegial.settings')
+
+application = get_wsgi_application()
